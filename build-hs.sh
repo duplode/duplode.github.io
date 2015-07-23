12792991cdf980bf2b541bab59c9c4cf4da548b9
Daniel Mlot <duplode_1@yahoo.com.br>
2015-07-23 04:42:45 -0300

    Migrating build system to stack.
    Using an arbitrary Stackage nightly in a real project is possibly not
    a good idea, but we can live with that here for the moment.

--- build-hs.sh
+++ build-hs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cabal configure
-cabal build
-cp dist/build/duplode-github-io/duplode-github-io .
+stack build # With stack 0.2, add the option --copy-bins.
+# This is horrible, but will improve with stack 0.2.
+cp .stack-work/install/x86_64-linux/nightly-2015-07-19/7.10.1/bin/duplode-github-io .
