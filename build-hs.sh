f2ef6829867d8683671a429e3c527a1dd80643b7
Daniel Mlot <duplode_1@yahoo.com.br>
2017-02-25 23:36:20 -0300

    Sanitising the old build-hs Bash script.

--- build-hs.sh
+++ build-hs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-stack build # With stack 0.2, add the option --copy-bins.
-# This is horrible, but will improve with stack 0.2.
-cp .stack-work/install/x86_64-linux/nightly-2015-07-19/7.10.1/bin/duplode-github-io .
+stack build
+cp $(stack path --local-install-root)/bin/duplode-github-io .
